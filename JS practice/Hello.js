4601ad1ad829fe3b0a7ef70e4fa9e945a0d4c2cd
Waleed Awan <waleedmalck2@gmail.com>
2024-10-22 15:30:58 +0500

    Your commit message

---
+++ JS practice/Hello.js
@@ -0,0 +1,12 @@
+
+function fahrenheitToCelsius(fahrenheit) {  
+    return (fahrenheit - 32) / 1.8;  
+}
+
+console.log("The value is Celsius:",fahrenheitToCelsius(50))
+
+function celsiusToFahrenheit(celsius) {  
+    return (celsius * 9/5) + 32 ;  
+}
+
+console.log("The value of forenheit is",celsiusToFahrenheit(10))
